eee7766576b9851d04333c0c98bd050bce0ed3ba
Dexyn <duev1473@gmail.com>
2025-08-16 21:42:14 -0500

    staging pull

--- super_shot.py
+++ super_shot.py
@@ -6,11 +6,10 @@ from constants import SUPER_SHOT_RADIUS
 class SuperShot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SUPER_SHOT_RADIUS)
-        self.velocity = 200
 
     def draw(self, screen):
         xy = SUPER_SHOT_RADIUS
         pygame.draw.rect(screen, "yellow", (xy,xy,xy,xy))
 
     def update(self, dt):
-        self.position.y += 200 * dt
+        self.velocity += 200 * dt FIX THIS
